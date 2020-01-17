29655c570e8b411094851ebb5bb0dd579bf832dd
DanielHGimenez <danielhgimenez027@gmail.com>
2020-01-16 22:03:11 -0300

    continuação

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 function App() {
+
+  const [ devs, setDevs ] = useState([]);
+  
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required></input>
-          </div>
 
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required></input>
-          </div>
-
-          <div className="input-group">
-            
-            <div className="input-block">
-              <label htmlFor="latitude">Usuário do Github</label>
-              <input name="latitude" id="latitude" required></input>
-            </div>
-            
-            <div className="input-block">
-              <label htmlFor="longetude">Usuário do Github</label>
-              <input name="longetude" id="longetude" required></input>
-            </div>
-
-          </div>
-
-          <button type="submit">Salvar</button>
-
-        </form>
+        <DevForm onSubmit={ handleAddDev } />
+      
       </aside>
-      <main></main>
+      <main>
+        <ul>
+
+          { 
+            devs.map(dev => (
+              <DevItem key={ dev._id } dev={ dev } />
+            )) 
+          }
+
+        </ul>
+      </main>
     </div>
   );
 }
